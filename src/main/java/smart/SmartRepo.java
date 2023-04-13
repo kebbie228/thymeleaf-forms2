9a7c8356a48275ad28e8ef3e8980f24f245ed20b
gk <a>
2023-04-13 18:46:44 +0300

    tf

---
+++ src/main/java/smart/SmartRepo.java
@@ -0,0 +1,8 @@
+package smart;
+
+import org.itstep.os.Os;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SmartRepo extends JpaRepository<Smart,Long> {
+
+}
---
+++ src/main/java/smart/SmartService.java
@@ -0,0 +1,31 @@
+package smart;
+
+import org.itstep.os.Os;
+import org.itstep.os.OsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SmartService {
+    @Autowired
+SmartRepo smartRepository;
+
+    public List<Smart> findAll() {
+        return smartRepository.findAll(); //список всех фирм
+    }
+
+    public Smart save(Smart smart) {
+        return smartRepository.save(smart);
+    }
+
+    public Smart findById(Long id) {
+        return smartRepository.findById(id).get();
+    }
+
+    public void deleteById(Long id) {
+        smartRepository.deleteById(id);
+    }
+}
+
